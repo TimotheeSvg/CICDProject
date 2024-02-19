4468ffd7410f6149cb6d3f4e6ea11051767f5509
TimotheeSvg <ssauvage.timothee@gmail.com>
2024-02-19 17:30:20 +0100

    init lint

--- server/index.js
+++ server/index.js
@@ -3,14 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log('Request user')
-    res.status(200).end();
+  res.status(200).end();
 });
-
 app.get('/test', (req, res) => {
-    res.send(['a', 'b', 'c']);
+  res.send(['a', 'b', 'c']);
 });
 
 app.listen(3000, () => {
-    console.log('Serveur démarré sur le port 3000');
+  console.log('listen')
 });
